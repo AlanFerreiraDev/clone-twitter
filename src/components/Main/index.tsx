988d0dd63bcdb5e41cd08228c1e6c4f899abdae3
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2021-07-05 20:55:14 -0300

    first commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Alan Bruno</strong>
+          <span>219 tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* <ProfilePage /> */}
+      {/* 
+      <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu> */}
+    </Container>
+  );
+};
+
+export default Main;
